ac1a592052bed3835f9598cc4be8956abcf5a988
fredo-cli <frederic.terrible@gmail.com>
2012-12-02 16:57:16 +0100

    test

--- imageTobase64MD.sh
+++ imageTobase64MD.sh
@@ -10,4 +10,4 @@ mimetype=$(file --mime-type -b $1)
 mimetype=$(echo "$mimetype"|sed 's/\//\\\//')
 echo mimetype=$mimetype
 
-uuencode -m $1 /dev/stdout | sed '1d' | sed '$d' | tr -d '\n' |sed "s/^/\!\[${filename}\]\(data:${mimetype};base64,/"  | sed 's/$/\)/' | xclip -selection clipboardimage
+uuencode -m $1 /dev/stdout | sed '1d' | sed '$d' | tr -d '\n' |sed "s/^/\!\[${filename}\]\[${filename}\]\n\[${filename}\]:data:${mimetype};base64,/"  | xclip -selection clipboardimage
