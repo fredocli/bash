1e308b4cd28abca80d477c22890f6e6386f0185e
fredo-cli <frederic.terrible@gmail.com>
2012-12-02 16:43:25 +0100

    test

--- imageTobase64MD.sh
+++ imageTobase64MD.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # get the name without extension
-
-filename=$(basename )
+filename=$(basename $1)
 filename=${filename%.*}
 echo filename=$filename
 
 # Get the mime-type
+mimetype=$(file --mime-type -b $1)
+mimetype=$(echo "$mimetype"|sed 's/\//\\\//')
+echo mimetype=$mimetype
 
-mimetype=$(file --mime-type -b )
-
-
-uuencode -m Desktop/logo.png /dev/stdout | sed '1d' | sed '$d' | tr -d '\n' |sed s/^/\!\[${filename}
-\]\(data:${minetype};base64,/  |sed s/$/\)/ | xclip -selection clipboardimage
+uuencode -m $1 /dev/stdout | sed '1d' | sed '$d' | tr -d '\n' |sed "s/^/\!\[${filename}\]\(data:${mimetype};base64,/"  | sed 's/$/\)/' | xclip -selection clipboardimage
